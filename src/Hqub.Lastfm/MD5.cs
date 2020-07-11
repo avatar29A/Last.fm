ebc01c5bed135dd7d0d1d79baaacbaa422e5acb3
wo80 <christian.woltering@tu-dortmund.de>
2020-07-11 14:16:51 +0200

    Add some helper classes.

---
+++ src/Hqub.Lastfm/MD5.cs
@@ -0,0 +1,54 @@
+﻿namespace Hqub.Lastfm
+{
+    using System.Text;
+
+    using CryptoMD5 = System.Security.Cryptography.MD5;
+
+    /// <summary>
+    /// MD5 helper class.
+    /// </summary>
+    public static class MD5
+    {
+        /// <summary>
+        /// Returns the MD5 hash of a string.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string ComputeHash(string text)
+        {
+            return ComputeHash(Encoding.UTF8.GetBytes(text));
+        }
+
+        /// <summary>
+        /// Returns the MD5 hash of a byte array.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string ComputeHash(byte[] data)
+        {
+            return ComputeHash(data, data.Length);
+        }
+
+        /// <summary>
+        /// Returns the MD5 hash of a byte array.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string ComputeHash(byte[] data, int size)
+        {
+            var hashAlgorithm = CryptoMD5.Create();
+
+            var buffer = hashAlgorithm.ComputeHash(data, 0, size);
+
+            var sb = new StringBuilder();
+
+            foreach (byte b in buffer)
+            {
+                sb.Append(b.ToString("x2").ToLower());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- src/Hqub.Lastfm/Utilities.cs
+++ src/Hqub.Lastfm/Utilities.cs
@@ -1,79 +1,91 @@
-// Utilities.cs
-//
-//  Copyright (C) 2008 Amr Hassan
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-//
-//
-
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Text;
-using System.Security.Cryptography;
-using System.Net;
-using System.Web;
-using Lastfm.Services;
-
-namespace Lastfm
+namespace Hqub.Lastfm
 {
-	/// <summary>
-	/// General utility functions
-	/// </summary>
-	public static class Utilities
-	{
-		internal static string UserAgent
-		{
-			get { return "lastfm-sharp/" + Lastfm.Lib.Version.ToString(); }
-		}
-		
-		/// <summary>
-		/// Returns the md5 hash of a string.
-		/// </summary>
-		/// <param name="text">
-		/// A <see cref="System.String"/>
-		/// </param>
-		/// <returns>
-		/// A <see cref="System.String"/>
-		/// </returns>
-		public static string md5(string text)
-		{
-			byte[] buffer = Encoding.UTF8.GetBytes(text);
-			
-			MD5CryptoServiceProvider c = new MD5CryptoServiceProvider();
-			buffer = c.ComputeHash(buffer);
-			
-			StringBuilder builder = new StringBuilder();
-			foreach(byte b in buffer)
-				builder.Append(b.ToString("x2").ToLower());
-			
-			return builder.ToString();
-		}
-		
-		public static DateTime TimestampToDateTime(long timestamp, DateTimeKind kind)
-		{
-			return new DateTime(1970, 1, 1, 0, 0, 0, 0, kind).AddSeconds(timestamp).ToLocalTime();
-		}
-		
-		public static long DateTimeToUTCTimestamp(DateTime dateTime)
-		{
-			DateTime baseDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-			
-			TimeSpan span = dateTime.ToUniversalTime() - baseDate;
-			
-			return (long)span.TotalSeconds;
-		}
-	}
+    using System;
+    using System.Net;
+
+    /// <summary>
+    /// General utility functions
+    /// </summary>
+    static class Utilities
+    {
+        public const string LASTFM_SECURE = "https://www.last.fm/";
+        public const string LASTFM = "http://www.last.fm/";
+
+        public static string SafeUrl(string text)
+        {
+            return WebUtility.UrlEncode(WebUtility.UrlEncode(text));
+        }
+
+        public static string GetSiteDomain(SiteLanguage language)
+        {
+            switch (language)
+            {
+                case SiteLanguage.English:
+                    return LASTFM_SECURE;
+                case SiteLanguage.German:
+                    return LASTFM_SECURE + "de/";
+                case SiteLanguage.Spanish:
+                    return LASTFM_SECURE + "es/";
+                case SiteLanguage.French:
+                    return LASTFM_SECURE + "fr/";
+                case SiteLanguage.Italian:
+                    return LASTFM_SECURE + "it/";
+                case SiteLanguage.Polish:
+                    return LASTFM_SECURE + "pl/";
+                case SiteLanguage.Portuguese:
+                    return LASTFM_SECURE + "pt/";
+                case SiteLanguage.Swedish:
+                    return LASTFM_SECURE + "sv/";
+                case SiteLanguage.Turkish:
+                    return LASTFM_SECURE + "tr/";
+                case SiteLanguage.Russian:
+                    return LASTFM_SECURE + "ru/";
+                case SiteLanguage.Japanese:
+                    return LASTFM_SECURE + "ja/";
+                case SiteLanguage.Chinese:
+                    return LASTFM_SECURE + "zh/";
+                default:
+                    break;
+            }
+
+            return LASTFM_SECURE;
+        }
+
+        public static DateTime TimestampToDateTime(long timestamp, DateTimeKind kind = DateTimeKind.Utc)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0, kind).AddSeconds(timestamp).ToLocalTime();
+        }
+
+        public static long DateTimeToUtcTimestamp(DateTime dateTime)
+        {
+            DateTime baseDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+            TimeSpan span = dateTime.ToUniversalTime() - baseDate;
+
+            return (long)span.TotalSeconds;
+        }
+
+        public static string GetPeriod(Period period)
+        {
+            switch (period)
+            {
+                case Period.Overall:
+                    return "overall";
+                case Period.SevenDay:
+                    return "7day";
+                case Period.OneMonth:
+                    return "1month";
+                case Period.ThreeMonth:
+                    return "3month";
+                case Period.SixMonth:
+                    return "6month";
+                case Period.TwelveMonth:
+                    return "12month";
+                default:
+                    break;
+            }
+
+            return "overall";
+        }
+    }
 }
